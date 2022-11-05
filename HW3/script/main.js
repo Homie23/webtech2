19e3f09efd7e1d223f4d4e768f122b6d0826f4c8
Aleksandr Solodovnikov <50180805+Homie23@users.noreply.github.com>
2022-11-05 20:28:43 +0200

    Добавил ДЗ3- localStorage
    localStorage в main.js

---
+++ HW3/script/main.js
@@ -0,0 +1,41 @@
+const key = 'currentTab';
+const menuBurger = document.querySelector(".burger-menu");
+menuBurger.addEventListener("click", function () {
+  menuBurger.classList.toggle('burger-menu_active');
+});
+
+document.querySelector('.burger-menu__nav').addEventListener('click', event => {
+  if (event.target.innerText.trim() === 'Buttons') {
+    delElements();
+    buttons();
+  }
+  if (event.target.innerText.trim() === 'Create table') {
+    delElements();
+    create_table_form();
+  }
+})
+
+function delElements() {
+  document.querySelectorAll('.divAdd div').forEach(function (a) {
+    a.remove()
+  })
+}
+
+function setStorage(key, val) {
+  localStorage.setItem(key, val);
+}
+
+function runStorage(key) {
+  let s = localStorage.getItem(key);
+  if(s === 'btn') {
+      buttons();
+  } else if(s === 'tab') {
+      create_table_form();
+  }
+}
+window.onload = (event) => {
+  runStorage(key);
+};
+
+
+
